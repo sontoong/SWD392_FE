3d3f731c73ce31418a59a578f7847c199865bb95
sontoong <vominhthang061003@gmail.com>
2024-01-17 20:43:46 +0700

    init source

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import "nprogress/nprogress.css";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
 import { Provider } from "react-redux";
+import App from "./App.tsx";
 import { store } from "./app/redux/store.ts";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
