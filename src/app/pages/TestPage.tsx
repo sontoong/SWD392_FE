eaaf9e2ebefc9625385cf2d088c198eca0deef53
sontoong <vominhthang061003@gmail.com>
2024-03-03 22:47:58 +0700

    add upload img

--- src/app/pages/TestPage.tsx
+++ src/app/pages/TestPage.tsx
@@ -17,7 +17,7 @@ export default function TestPage() {
     // <EnterpriseModalEditGeneralInfo/>
     // <EnterpriseDetailPage/>
     // <FreelancerProjectList/>
-    <FreeLancerIncomeList/>
+    <FreeLancerIncomeList />
     // <ViewSignContract/>
   );
 }
